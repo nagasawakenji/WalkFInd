e887d64899a6b551f38cc4646937c4624c347255
ishiduka <akito1112ishiduka@icloud.com>
2025-11-08 16:37:57 +0900

    Initial commit of WalkFind project structure

---
+++ src/test/java/nagasawakenji/WalkFind/WalkFindApplicationTests.java
@@ -0,0 +1,13 @@
+package nagasawakenji.WalkFind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WalkFindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
