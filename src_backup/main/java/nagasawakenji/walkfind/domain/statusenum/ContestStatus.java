60564a4b5967c44a8c22153758693696057f54e3
ishiduka <akito1112ishiduka@icloud.com>
2025-11-15 12:04:01 +0900

    delete src_backup

--- src_backup/main/java/nagasawakenji/walkfind/domain/statusenum/ContestStatus.java
+++
@@ -1,8 +0,0 @@
-package nagasawakenji.walkfind.domain.statusenum;
-
-public enum ContestStatus {
-    UPCOMING,       // 開催前（投稿受付開始前）
-    IN_PROGRESS,    // 開催中（投稿受付中、投票受付中）
-    CLOSED_VOTING,  // 投稿・投票期間終了（結果集計・審査待ち）
-    ANNOUNCED      // 結果発表済（コンテスト完了）
-}
--- src_backup/main/java/nagasawakenji/walkfind/domain/statusenum/UserRole.java
+++
@@ -1,6 +0,0 @@
-package nagasawakenji.walkfind.domain.statusenum;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
