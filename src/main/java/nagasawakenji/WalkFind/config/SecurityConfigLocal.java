735e9c104638e9cfeea2cc62fb9f7bfb4bd52c11
ishiduka <akito1112ishiduka@icloud.com>
2025-11-17 14:15:30 +0900

    tmp commit

---
+++ src/main/java/nagasawakenji/WalkFind/config/SecurityConfigLocal.java
@@ -0,0 +1,33 @@
+package nagasawakenji.walkfind.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@Profile("local")
+public class SecurityConfigLocal {
+
+    @Bean
+    public SecurityFilterChain securityFilterChainLocal(HttpSecurity http) throws Exception {
+
+        http
+                .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(csrf -> csrf.disable())
+                .cors(Customizer.withDefaults())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> {
+                    jwt.jwkSetUri("http://localhost/dummy/jwks.json");
+                }))
+
+                .authorizeHttpRequests(auth -> auth
+                        // ローカルは全部許可して良い
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
