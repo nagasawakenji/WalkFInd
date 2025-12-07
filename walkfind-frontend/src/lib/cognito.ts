0ef83df2b042bc0cea1f41180511907169020d93
ishiduka <akito1112ishiduka@icloud.com>
2025-12-07 23:40:04 +0900

    modify url to run correctly when environmental value not read

--- walkfind-frontend/src/lib/cognito.ts
+++
@@ -1,9 +0,0 @@
-// cognitoのurl
-export const COGNITO_LOGIN_URL =
-  "https://walkfind-auth.auth.ap-northeast-1.amazoncognito.com/login" +
-  "?client_id=3n38j4erbgfcanu6v9n87he38r" +
-  "&response_type=code" +
-  "&scope=email+openid+phone" + 
-  "&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fauth%2Fcallback";
-
-  
