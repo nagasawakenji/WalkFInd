863986a5f14b9cc0e11ff9ddeef6c24d5ef84909
ishiduka <akito1112ishiduka@icloud.com>
2025-12-17 21:46:54 +0900

    modify mapper.xml to avoid failing mapping contestStatus

--- walkfind-lambda/src/main/java/nagasawakenji/walkfind/handler/GlobalExceptionHandler.java
+++ walkfind-lambda/src/main/java/nagasawakenji/walkfind/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package nagasawakenji.walkfind.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import nagasawakenji.walkfind.exception.ApiError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,10 +8,12 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiError> handle(Exception e) {
+        log.error("Unhandled exception", e);
         return ResponseEntity.status(500)
                 .body(new ApiError(e.getMessage(), "予期せぬサーバーエラーが発生しました"));
     }
